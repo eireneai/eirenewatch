65eec48d5495771135d02fd75e8facbcba588495
ayoung4 <alfredsyoung@gmail.com>
2023-08-31 15:54:57 -0700

    fix: export necessary types

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ export { ChokidarWatcher } from './backends/ChokidarWatcher';
 export { FileWatchingBackend } from './backends/FileWatchingBackend';
 export { FSWatcher } from './backends/FSWatcher';
 export { TurboWatcher } from './backends/TurboWatcher';
-export { type ChangeEvent, type Expression, type TriggerInput } from './types';
 export { type ProcessPromise } from 'zx';
 
+export * from './mod/types';
 export { defineConfigWatcher } from './mod/defineConfigWatcher';
 export { watch } from './mod/watch';
